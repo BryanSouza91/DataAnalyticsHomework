9dd068203f14b60111e599bfcb696cee5fe1804b
BryanSouza91 <40864284+BryanSouza91@users.noreply.github.com>
2020-02-04 18:34:31 -0800

    Updated D3 HW Week 16

--- D3-challenge/assets/js/app.js
+++ D3-challenge/assets/js/app.js
@@ -4,8 +4,8 @@
 function buildScatterChart() {
 
     // set the dimensions and margins of the graph
-    var margin = { top: 10, right: 30, bottom: 30, left: 60 },
-        width = 760 - margin.left - margin.right,
+    var margin = { top: 10, right: 30, bottom: 60, left: 175 },
+        width = 1200 - margin.left - margin.right,
         height = 600 - margin.top - margin.bottom;
 
     // append the svg object to the body of the page
@@ -18,12 +18,12 @@ function buildScatterChart() {
             "translate(" + margin.left + "," + margin.top + ")");
 
     // Pull in the CSV data
-    let csvFile = "/Homework/assets/data/data.csv";
+    let csvFile = "/Homework_Week-16/assets/data/data.csv";
     d3.csv(csvFile).then((data) => {
 
         // Add X axis
         var x = d3.scaleLinear()
-            .domain([30, 46])
+            .domain([8, 30])
             .range([0, width]);
         svg.append("g")
             .attr("transform", "translate(0," + height + ")")
@@ -42,9 +42,9 @@ function buildScatterChart() {
             .data(data)
             .enter()
             .append("circle")
-            .attr("cx", (d => x(d.age) ))
+            .attr("cx", (d => x(d.smokes) ))
             .attr("cy", (d => y(d.income) ))
-            .attr("r", 15 )                            // (d => (d.poverty) )
+            .attr("r", 15 )
             .style("fill", "#69b3a2")
 
         svg.append("g")
@@ -54,23 +54,33 @@ function buildScatterChart() {
             .append("text")
             .text(d => d.abbr)
             .attr("font-size", 12)
-            .attr("dx", (d => x(d.age) - 6 ))
+            .attr("dx", (d => x(d.smokes) - 6 ))
             .attr("dy", (d => y(d.income) + 6 ))
             .style("fill", "white");
 
+        svg.append("text")
+            .attr("x", -160)
+            .attr("y", (height / 2) - 20)
+            .attr("font-size", 24)
+            .text("Median");
+        
+        svg.append("text")
+            .attr("x", -160)
+            .attr("y", (height / 2))
+            .attr("font-size", 24)
+            .text("Household");
 
         svg.append("text")
-            .attr("x", (-height / 2))
-            .attr("y", -42)
-            .attr("transform", "rotate(-90)")
-            .attr("font-size", 18)
-            .text("Age");
+            .attr("x", -160)
+            .attr("y", (height / 2) + 20)
+            .attr("font-size", 24)
+            .text("Income");
 
         svg.append("text")
-            .attr("x", ((width / 2) + 10 ))
-            .attr("y", height + 20)
-            .attr("font-size", 18)
-            .text("Income")
+            .attr("x", ((width / 2) - 40))
+            .attr("y", height + 45)
+            .attr("font-size", 24)
+            .text("% population that Smokes")
         
     });
 }
@@ -79,7 +89,7 @@ function buildScatterChart() {
 
 function init() {
     // Pull in the CSV data
-    let csvFile = "/Homework/assets/data/data.csv";
+    let csvFile = "/Homework_Week-16/assets/data/data.csv";
     d3.csv(csvFile).then((data) => {
 
         // Set a default to build the initial plots
